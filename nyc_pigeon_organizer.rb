8ca3620701459f45aecc8ef49dbae33a07a72922
Sam Kyprianou <samuelkyprianou@gmail.com>
2019-11-17 17:09:02 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,7 +5,8 @@ def nyc_pigeon_organizer(data)
   data.each do |att, key|
     key.each do |stats, array_names|
       array_names.each do |name|
-        if pigeon_list
+        if !pigeon_list[name]
+          pigeon
     binding.pry
   end
   end
