08257a38a69b9c50b6029e2b303ee8a7986cbb86
Sam Kyprianou <samuelkyprianou@gmail.com>
2019-11-17 17:00:49 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,8 +3,10 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
   data.each do |att, key|
-    key.each do |stats, names|
+    key.each do |stats, array_names|
+      arrays.each do |name|
     binding.pry
+  end
   end
   end
 end
