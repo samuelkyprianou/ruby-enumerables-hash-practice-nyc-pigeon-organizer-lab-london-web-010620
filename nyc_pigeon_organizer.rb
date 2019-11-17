7c04c6ef2e0aa2fd2891830a9e0703f30c23874c
Sam Kyprianou <samuelkyprianou@gmail.com>
2019-11-17 17:23:19 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |att, key|
+    key.each do |stats, array_names|
+      array_names.each do |name|
+         pigeon_list[name] ||= {}
+        pigeon_list[name][att] ||= []
+        pigeon_list[name][att].push(stats.to_s)
+      end
+    end 
+  end
+  pigeon_list
 end
